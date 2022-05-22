7734432a7853e1f084275dee5a8dd70b136c23dd
Ivan <arturarutiunian@iMac.local>
2022-05-22 09:34:03 +0300

    feat: Handle network errors

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController+Networking.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController+Networking.swift
@@ -27,23 +27,23 @@ struct Profile: Codable {
 
 extension AccountSummaryViewController {
     func fetchProfile(forUserId userId: String, completion: @escaping (Result<Profile,NetworkError>) -> Void) {
-        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)")!
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                guard let data = data, error == nil else { // This block of code...
+//        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/bankey/profile/\(userId)")!
+//
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            DispatchQueue.main.async {
+//                guard let data = data, error == nil else { // This block of code...
                     completion(.failure(.serverError))
-                    return
-                }
-                
-                do {
-                    let profile = try JSONDecoder().decode(Profile.self, from: data)
-                    completion(.success(profile))
-                } catch {
-                    completion(.failure(.decodingError))
-                } //... runs asynchronously on a background thread...
-            }
-        }.resume()
+//                    return
+//                }
+//
+//                do {
+//                    let profile = try JSONDecoder().decode(Profile.self, from: data)
+//                    completion(.success(profile))
+//                } catch {
+//                    completion(.failure(.decodingError))
+//                } //... runs asynchronously on a background thread...
+//            }
+//        }.resume()
     }
 }
 
